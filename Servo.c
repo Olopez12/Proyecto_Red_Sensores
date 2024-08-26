7a9d42f6e51c1c4cd06f9ce5306da12fe4360842
Olopez12 <146391704+Olopez12@users.noreply.github.com>
2024-08-25 18:48:56 -0600

    Add files via upload

---
+++ Servo.c
@@ -0,0 +1,34 @@
+/*
+ * Servo.c
+ *
+ * Created: 13/08/2024 23:20:23
+ *  Author: Usuario
+ */ 
+
+#include "Servo.h"
+
+void timer1_init(void) {
+	// Configurar el pin OC1A (PB1) como salida
+	DDRB |= (1 << PINB1);
+
+	// Configurar Timer1 en modo Fast PWM con ICR1 como top
+	TCCR1A |= (1 << WGM11) | (1 << COM1A1); // Modo 14, Clear OC1A on Compare Match, set OC1A at BOTTOM
+	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler de 8
+
+	// Configurar ICR1 para un periodo de 20ms (50Hz) (F_CPU/(Prescaler * F_PWM) - 1)
+	ICR1 = 39999; // 16MHz / (8 * 50Hz) - 1 = 39999
+
+	// Iniciar el servomotor en ángulo 0° (duty cycle de 1ms)
+	OCR1A = 1999; // 1ms * 50Hz = 1999
+}
+
+
+
+// Función para mover el servomotor a 0° o 90°
+void mover_servomotor(uint8_t angulo) {
+	if (angulo == 0) {
+		OCR1A = 1999; // 1ms para 0°
+		} else if (angulo == 90) {
+		OCR1A = 4999; // 2ms para 90°
+	}
+}
---
+++ Servo.h
@@ -0,0 +1,20 @@
+/*
+ * Servo.h
+ *
+ * Created: 13/08/2024 23:22:44
+ *  Author: Usuario
+ */ 
+
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+void timer1_init(void);
+// Función para mover el servomotor a 0° o 90°
+void mover_servomotor(uint8_t angulo);
+#endif /* SERVO_H_ */
